ff4277a98dc0368c0e5018bcaf4558cac9a4a5d4
crashoverburn <maiquelrinco@hotmail.com>
2020-07-27 01:25:43 +0100

    hello change

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
 
 def hello ():  
-    label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
+    label1 = tk.Label(root, text= 'Hello World! Learning how to use python', fg='green', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 200, window=label1)
     
 button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
